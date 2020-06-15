38b6e92e7aca97b3cd20d4bb77ab4809575404ad
Roman Komarevtsev <hellromul@gmail.com>
2020-06-15 03:23:24 +0300

    Added new stringify method for MediaFile

--- core/src/main/kotlin/io/github/iromul/media/library/collection/stringify.kt
+++ core/src/main/kotlin/io/github/iromul/media/library/collection/stringify.kt
@@ -12,4 +12,20 @@ fun MediaCollection.stringify(): String {
     return "$typeString $name"
 }
 
-fun MediaFile.stringify() = "$artist - $album - $title (track $track)"
+fun MediaFile.stringify(includeTrack: Boolean = false, includeFile: Boolean = false): String {
+    val minimal = listOf(artist, album, title).joinToString(separator = " - ") { it.notNullOrBlankOrElse("<N/A>") }
+
+    var result = minimal
+
+    if (includeTrack) {
+        result = "$track. $result"
+    }
+
+    if (includeFile) {
+        result += " (file $file)"
+    }
+
+    return result
+}
+
+private fun String?.notNullOrBlankOrElse(default: String) = if (isNullOrBlank()) default else this!!
